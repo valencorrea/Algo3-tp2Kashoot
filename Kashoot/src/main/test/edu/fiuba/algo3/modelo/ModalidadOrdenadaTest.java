9dc90ad086c9472029f763e698b05ccf8e2345a0
agustina <asegura@fi.uba.ar>
2020-08-06 16:03:15 -0300

    se agregaron pruebas de la clase modalidadOrdenada

---
+++ Kashoot/src/main/test/edu/fiuba/algo3/modelo/ModalidadOrdenadaTest.java
@@ -0,0 +1,55 @@
+package edu.fiuba.algo3.modelo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ModalidadOrdenadaTest {
+
+    @Test
+    public void dadaUnaModalidadOrdenadaCuandoUnJugadorRespondeBienEntoncesSuPuntajeSeraElCorrecto(){
+
+        Opcion opcion1 = new Opcion("opcion1");
+        Opcion opcion2 = new Opcion("opcion2");
+        Opcion opcion3 = new Opcion("opcion3");
+
+        Respuesta respuestaCorrecta = new Respuesta();
+        respuestaCorrecta.agregarOpcion(opcion1);
+        respuestaCorrecta.agregarOpcion(opcion2);
+        respuestaCorrecta.agregarOpcion(opcion3);
+
+        Respuesta respuestaUsuario = new Respuesta();
+        respuestaUsuario.agregarOpcion(opcion1);
+        respuestaUsuario.agregarOpcion(opcion2);
+        respuestaUsuario.agregarOpcion(opcion3);
+
+        ModalidadOrdenada modalidadOrdenada = new ModalidadOrdenada();
+        int resultado = modalidadOrdenada.calcularPuntaje(respuestaUsuario,respuestaCorrecta);
+
+        assertEquals(10,resultado);
+
+    }
+    @Test
+    public void dadaUnaModalidadOrdenadaCuandoUnJugadorRespondeMalEntoncesSuPuntajeSeraElCorrecto(){
+
+        Opcion opcion1 = new Opcion("opcion1");
+        Opcion opcion2 = new Opcion("opcion2");
+        Opcion opcion3 = new Opcion("opcion3");
+
+        Respuesta respuestaCorrecta = new Respuesta();
+        respuestaCorrecta.agregarOpcion(opcion1);
+        respuestaCorrecta.agregarOpcion(opcion2);
+        respuestaCorrecta.agregarOpcion(opcion3);
+
+        Respuesta respuestaUsuario = new Respuesta();
+        respuestaUsuario.agregarOpcion(opcion1);
+        respuestaUsuario.agregarOpcion(opcion3);
+        respuestaUsuario.agregarOpcion(opcion2);
+
+        ModalidadOrdenada modalidadOrdenada = new ModalidadOrdenada();
+        int resultado = modalidadOrdenada.calcularPuntaje(respuestaUsuario,respuestaCorrecta);
+
+        assertEquals(0,resultado);
+
+    }
+}
