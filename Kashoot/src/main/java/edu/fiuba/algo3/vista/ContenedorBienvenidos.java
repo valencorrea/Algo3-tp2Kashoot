22c6a1c672ce22112bea153298a0cdabe985c99d
valen <vcorrea@fi.uba.ar>
2020-08-15 18:15:58 -0300

    agrando botones y letra de la bienvenida :D

--- Kashoot/src/main/java/edu/fiuba/algo3/vista/ContenedorBienvenidos.java
+++ Kashoot/src/main/java/edu/fiuba/algo3/vista/ContenedorBienvenidos.java
@@ -33,20 +33,29 @@ public class ContenedorBienvenidos extends VBox {
             BackgroundImage imagenDeFondo = new BackgroundImage(imagen, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
             this.setBackground(new Background(imagenDeFondo));
 
-            var labelBienvenida = new Label("Bienvenidos al kashoot!");
-            labelBienvenida.setFont(Font.font("Tahoma", FontWeight.BOLD, 30));
+            var labelBienvenida = new Label("¡Bienvenidos al kashoot!");
+            labelBienvenida.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 80));
             labelBienvenida.setTextFill(Color.HOTPINK);
 
-            var labelIngresarNombre = new Label("ingresen sus nombres para comenzar");
-            labelIngresarNombre.setFont(Font.font("Tahoma", FontWeight.BOLD, 18));
+            var labelIngresarNombre = new Label("Ingresen sus nombres para comenzar");
+            labelIngresarNombre.setFont(Font.font("Tahoma", FontWeight.EXTRA_LIGHT, 45));
             labelIngresarNombre.setTextFill(Color.web("#66A7C5"));
+
+            VBox contenedorEtiquetas = new VBox(labelBienvenida, labelIngresarNombre);
+            contenedorEtiquetas.setSpacing(170);
+            contenedorEtiquetas.setAlignment(Pos.CENTER);
 
             TextField textoJugador1 = new TextField();
             textoJugador1.setPromptText("Nombre del jugador 1");
             textoJugador1.setAlignment(Pos.CENTER);
+            textoJugador1.setScaleX(1.5);
+            textoJugador1.setScaleY(1.5);
+
             TextField textoJugador2 = new TextField();
             textoJugador2.setPromptText("Nombre del jugador 2");
             textoJugador2.setAlignment(Pos.CENTER);
+            textoJugador2.setScaleX(1.5);
+            textoJugador2.setScaleY(1.5);
 
             Jugador jugador1 = new Jugador(textoJugador1.getText());
             Jugador jugador2 = new Jugador(textoJugador2.getText());
@@ -54,22 +63,26 @@ public class ContenedorBienvenidos extends VBox {
             kashoot.agregarJugadores(jugador1, jugador2);
 
             HBox contenedorDeNombres = new HBox(textoJugador1, textoJugador2);
-            contenedorDeNombres.setSpacing(100);
+            contenedorDeNombres.setSpacing(200);
             contenedorDeNombres.setAlignment(Pos.CENTER);
 
             Button botonJugar = new Button();
-            botonJugar.setText("Jugar");
+            botonJugar.setText("JUGAR");
+            //botonJugar.setScaleX(2);
+            //botonJugar.setScaleY(2);
+            botonJugar.setPrefSize(200, 80);
+
 
             Label mensajeError = new Label();
             mensajeError.setAlignment(Pos.CENTER);
+            mensajeError.setFont(Font.font("Tahoma", FontWeight.BLACK, 15));
             contenedorDeNombres.getChildren().add(mensajeError);
-
 
             BotonJugarEventHandler botonJugarEventHandler = new BotonJugarEventHandler(stage, proximaEscena, textoJugador1, textoJugador2, mensajeError);
 
             botonJugar.setOnAction(botonJugarEventHandler);
 
-            this.getChildren().addAll(labelBienvenida, labelIngresarNombre, contenedorDeNombres,mensajeError, botonJugar);
+            this.getChildren().addAll(contenedorEtiquetas, contenedorDeNombres, mensajeError, botonJugar);
 
         }
 
