870e45eeb76dcd1589854a6f2fadb75162942c72
sol <msfontenla@fi.uba.ar>
2020-08-15 20:02:22 -0300

    Agregamos la clase Ronda

--- Kashoot/src/main/java/edu/fiuba/algo3/modelo/Kashoot.java
+++ Kashoot/src/main/java/edu/fiuba/algo3/modelo/Kashoot.java
@@ -2,7 +2,6 @@ package edu.fiuba.algo3.modelo;
 
 
 import edu.fiuba.algo3.vista.ModeloObservable;
-
 import java.util.*;
 
 
@@ -10,36 +9,58 @@ public class Kashoot  {
 
     private int tamanio = 20;
     ModeloObservable modelo;
-    private ArrayList<PreguntasYOpciones> preguntasYOpciones;
-    private Jugador jugador1;
-    private Jugador jugador2;
-
+    private ArrayList<Ronda> rondas = new ArrayList<Ronda>();
+    private ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+    private ListIterator jugadorActual = jugadores.listIterator();
+    //private Jugador jugador1;
+    //private Jugador jugador2;
+    //private Jugador jugadorActual;
+    // private ArrayList<PreguntasYOpciones> preguntasYOpciones;
+/*
     public Kashoot(ArrayList<PreguntasYOpciones> unasPreguntasYOpciones){
         //NOS FALTA RECIBIR TMB LOS JUGADORES
         this.preguntasYOpciones = unasPreguntasYOpciones;
     }
-
+*/
     private void inicializarModelo() {
 
         modelo = new ModeloObservable();
     }
 
     public ArrayList<Opcion> getOpciones() {// antes llamar a getpregunta
-        PreguntasYOpciones preguntaYOpciones = this.preguntasYOpciones.get(preguntasYOpciones.size()-1);
-        ArrayList<Opcion> opciones = preguntaYOpciones.getOpciones();
-        this.preguntasYOpciones.remove(preguntasYOpciones.size()-1);
+        Ronda ronda = this.rondas.get(rondas.size()-1);
+        ArrayList<Opcion> opciones = ronda.getOpciones();
         return opciones;
     }
 
     public Pregunta getPregunta() {
-        PreguntasYOpciones preguntaYOpciones = this.preguntasYOpciones.get(preguntasYOpciones.size()-1);
-        Pregunta pregunta = preguntaYOpciones.getPregunta();
+        Ronda ronda = this.rondas.get(rondas.size()-1);
+        Pregunta pregunta = ronda.getPregunta();
         return pregunta;
     }
 
     public void agregarJugadores(Jugador jugador1, Jugador jugador2) {
+        this.jugadores.add(jugador1);
+        this.jugadores.add(jugador2);
 
-        this.jugador1 = jugador1;
-        this.jugador2 = jugador2;
+       // this.jugador1 = jugador1;
+       // this.jugador2 = jugador2;
+    }
+//cambiar esta medio rancio
+    public Jugador obtenerJugadorActual(){
+
+        if(jugadorActual.hasNext()){
+
+            return (Jugador) this.jugadorActual.next();
+
+        }
+        this.jugadorActual.next();
+        return (Jugador) this.jugadorActual.previous();
+    }
+
+    public void agregarRonda(Ronda ronda) {
+
+        this.rondas.add(ronda);
+
     }
 }
