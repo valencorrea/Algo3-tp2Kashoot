2238b0ed6fd3b498027b9593407ac59ec954b581
agustina <asegura@fi.uba.ar>
2020-08-17 15:30:35 -0300

    agrego rondas al modelo

--- Kashoot/src/main/java/edu/fiuba/algo3/vista/Juego.java
+++ Kashoot/src/main/java/edu/fiuba/algo3/vista/Juego.java
@@ -2,8 +2,10 @@ package edu.fiuba.algo3.vista;
 
 import edu.fiuba.algo3.modelo.*;
 import edu.fiuba.algo3.modelo.formatos.Clasico;
+import edu.fiuba.algo3.modelo.formatos.Parcial;
 import edu.fiuba.algo3.modelo.formatos.Penalidad;
 import edu.fiuba.algo3.modelo.modalidades.ModalidadSinOrden;
+import edu.fiuba.algo3.modelo.modalidades.ModalidadOrdenada;
 import edu.fiuba.algo3.vista.ContenedorBienvenidos;
 import edu.fiuba.algo3.vista.ContenedorPreguntas;
 import javafx.application.Application;
@@ -35,14 +37,14 @@ import java.util.ArrayList;
 
         /* música */
 
-       // Media mp3MusicFile = new Media(new File("Kashoot/resources/mipan.mp3").toURI().toString());
+        Media mp3MusicFile = new Media(new File("Kashoot/resources/mipan.mp3").toURI().toString());
 
-       // MediaPlayer musica = new MediaPlayer(mp3MusicFile);
-      //  musica.setAutoPlay(true);
-       // musica.setOnEndOfMedia(() -> musica.seek(Duration.ZERO));
-       // musica.setVolume(0.1);
-       // musica.play();
-        /**/
+        MediaPlayer musica = new MediaPlayer(mp3MusicFile);
+        musica.setAutoPlay(true);
+        musica.setOnEndOfMedia(() -> musica.seek(Duration.ZERO));
+        musica.setVolume(0.1);
+        musica.play();
+
 
         stage.setTitle("Kashoot");
 
@@ -65,42 +67,205 @@ import java.util.ArrayList;
     private Kashoot crearModelo() {
 
         //ArrayList <PreguntasYOpciones> preguntasYOpciones = leerArchivo("resources/archivo.txt");
+        //multipleChoice; Clasico; de que color es el cielo?;rojo;mostaza;rosita;/celeste
+        Opcion opcionCorrecta11 = new Opcion("Celeste");
+        Opcion opcionIncorrecta11 = new Opcion("rojo");
+        Opcion opcionIncorrecta12 = new Opcion("Rosita");
+        Opcion opcionIncorrecta13 = new Opcion("Mostaza");
 
-        Opcion opcionCorrecta = new Opcion("Verdadero");
-        Opcion opcionIncorrecta = new Opcion("Falsa");
 
-        ArrayList <Opcion> opciones = new ArrayList<Opcion>();
-        opciones.add(opcionCorrecta);
-        opciones.add(opcionIncorrecta);
+        ArrayList <Opcion> opciones1 = new ArrayList<Opcion>();
+        opciones1.add(opcionCorrecta11);
+        opciones1.add(opcionIncorrecta11);
+        opciones1.add(opcionIncorrecta12);
+        opciones1.add(opcionIncorrecta13);
 
-        Respuesta respuestaCorrecta = new Respuesta();
-        respuestaCorrecta.agregarOpcion(opcionCorrecta);
-        Pregunta unaPregunta = new Pregunta(new ModalidadSinOrden(new Clasico()), "Soy una pregunta?", respuestaCorrecta);
+        Respuesta respuestaCorrecta1 = new Respuesta();
+        respuestaCorrecta1.agregarOpcion(opcionCorrecta11);
+        Pregunta unaPregunta1 = new Pregunta(new ModalidadSinOrden(new Clasico()), "de que color es el cielo", respuestaCorrecta1);
 
         Ronda ronda1 = new Ronda();
-        ronda1.agregarPregunta(unaPregunta);
-        ronda1.agregarOpciones(opciones);
+        ronda1.agregarPregunta(unaPregunta1);
+        ronda1.agregarOpciones(opciones1);
     //-------------------------------------------//
-        Opcion opcionCorrecta1 = new Opcion("opcion1");
-        Opcion opcionIncorrecta2 = new Opcion("opcion2");
-        Opcion opcionIncorrecta3 = new Opcion("opcion3");
+        //multipleChoice; Penalidad; que numeros son pares?;7;pi;e;/2
+
+        Opcion opcionCorrecta21 = new Opcion("2");
+        Opcion opcionIncorrecta21 = new Opcion("7");
+        Opcion opcionIncorrecta22 = new Opcion("Pi");
+        Opcion opcionCorrecta22 = new Opcion("4");
+        Opcion opcionIncorrecta23 = new Opcion("e");
+
 
         ArrayList <Opcion> opciones2 = new ArrayList<Opcion>();
-        opciones2.add(opcionCorrecta1);
-        opciones2.add(opcionIncorrecta2);
-        opciones2.add(opcionIncorrecta3);
+        opciones2.add(opcionCorrecta21);
+        opciones2.add(opcionIncorrecta21);
+        opciones2.add(opcionIncorrecta22);
+        opciones2.add(opcionIncorrecta23);
+        opciones2.add(opcionCorrecta22);
+
 
         Respuesta respuestaCorrecta2 = new Respuesta();
-        respuestaCorrecta2.agregarOpcion(opcionCorrecta1);
-        Pregunta unaPregunta2 = new Pregunta(new ModalidadSinOrden(new Clasico()), "de que color es el cielo?", respuestaCorrecta2);
+        respuestaCorrecta2.agregarOpcion(opcionCorrecta21);
+        respuestaCorrecta2.agregarOpcion(opcionCorrecta22);
+        Pregunta unaPregunta2 = new Pregunta(new ModalidadSinOrden(new Penalidad()), "Que numeros son pares?", respuestaCorrecta2);
 
         Ronda ronda2 = new Ronda();
         ronda2.agregarPregunta(unaPregunta2);
         ronda2.agregarOpciones(opciones2);
+        //-------------------------------------------//
+        //verdaderoFalso; Clasico; c es terrible lenguaje?;/verdadero;falso
+
+        Opcion opcionCorrecta31 = new Opcion("Verdadero");
+        Opcion opcionIncorrecta31 = new Opcion("Falso");
+
+        ArrayList <Opcion> opciones3 = new ArrayList<Opcion>();
+        opciones3.add(opcionCorrecta31);
+        opciones3.add(opcionIncorrecta31);
+
+        Respuesta respuestaCorrecta3 = new Respuesta();
+        respuestaCorrecta3.agregarOpcion(opcionCorrecta31);
+
+        Pregunta unaPregunta3 = new Pregunta(new ModalidadSinOrden(new Clasico()), "C es un lenguaje terrible?", respuestaCorrecta3);
+
+        Ronda ronda3 = new Ronda();
+        ronda3.agregarPregunta(unaPregunta3);
+        ronda3.agregarOpciones(opciones3);
+
+        //-------------------------------------------//
+        //orderedChoice; Ordenar los sig numeros;3;6;7;9
+        Opcion opcionCorrecta41 = new Opcion("3");
+        Opcion opcionCorrecta42 = new Opcion("6");
+        Opcion opcionCorrecta43 = new Opcion("7");
+        Opcion opcionCorrecta44 = new Opcion("9");
+
+
+        ArrayList <Opcion> opciones4 = new ArrayList<Opcion>();
+
+        opciones4.add(opcionCorrecta43);
+        opciones4.add(opcionCorrecta41);
+        opciones4.add(opcionCorrecta44);
+        opciones4.add(opcionCorrecta42);
+
+
+
+        Respuesta respuestaCorrecta4 = new Respuesta();
+        respuestaCorrecta4.agregarOpcion(opcionCorrecta41);
+        respuestaCorrecta4.agregarOpcion(opcionCorrecta42);
+        respuestaCorrecta4.agregarOpcion(opcionCorrecta43);
+        respuestaCorrecta4.agregarOpcion(opcionCorrecta44);
+
+        Pregunta unaPregunta4 = new Pregunta(new ModalidadOrdenada(), "Seleccionar los numeros de forma acendente?", respuestaCorrecta4);
+
+        Ronda ronda4 = new Ronda();
+        ronda4.agregarPregunta(unaPregunta4);
+        ronda4.agregarOpciones(opciones4);
+
+        //-------------------------------------------//
+        //multipleChoice; Parcial; cual es la mejor faq?;fmed;/fiuba;/fiuba en otro boton;sociales
+
+        Opcion opcionCorrecta51 = new Opcion("Fiuba");
+        Opcion opcionIncorrecta51 = new Opcion("Fmediciona");
+        Opcion opcionIncorrecta52 = new Opcion("Sociales");
+        Opcion opcionCorrecta52 = new Opcion("Fiuba en otro boton");
+        Opcion opcionIncorrecta53 = new Opcion("Derecho");
+
+
+        ArrayList <Opcion> opciones5 = new ArrayList<Opcion>();
+        opciones5.add(opcionCorrecta51);
+        opciones5.add(opcionIncorrecta51);
+        opciones5.add(opcionIncorrecta52);
+        opciones5.add(opcionCorrecta52);
+        opciones5.add(opcionIncorrecta53);
+
+        Respuesta respuestaCorrecta5 = new Respuesta();
+        respuestaCorrecta5.agregarOpcion(opcionCorrecta51);
+        respuestaCorrecta5.agregarOpcion(opcionCorrecta52);
+        Pregunta unaPregunta5 = new Pregunta(new ModalidadSinOrden(new Parcial()), "Cual es la mejor facultad?", respuestaCorrecta5);
+
+        Ronda ronda5 = new Ronda();
+        ronda5.agregarPregunta(unaPregunta5);
+        ronda5.agregarOpciones(opciones5);
+
+        //-------------------------------------------//
+       // orderedChoice; Ordenar las sigueintes letras alfabeticamente;M;S;V;A;R
+
+        Opcion opcionCorrecta61 = new Opcion("A");
+        Opcion opcionCorrecta62 = new Opcion("M");
+        Opcion opcionCorrecta63 = new Opcion("R");
+        Opcion opcionCorrecta64 = new Opcion("S");
+        Opcion opcionCorrecta65 = new Opcion("V");
+
+        ArrayList <Opcion> opciones6 = new ArrayList<Opcion>();
+
+        opciones6.add(opcionCorrecta63);
+        opciones6.add(opcionCorrecta65);
+        opciones6.add(opcionCorrecta61);
+        opciones6.add(opcionCorrecta64);
+        opciones6.add(opcionCorrecta62);
+
+        Respuesta respuestaCorrecta6 = new Respuesta();
+        respuestaCorrecta6.agregarOpcion(opcionCorrecta61);
+        respuestaCorrecta6.agregarOpcion(opcionCorrecta62);
+        respuestaCorrecta6.agregarOpcion(opcionCorrecta63);
+        respuestaCorrecta6.agregarOpcion(opcionCorrecta64);
+        respuestaCorrecta6.agregarOpcion(opcionCorrecta65);
+
+        Pregunta unaPregunta6 = new Pregunta(new ModalidadOrdenada(), "Seleccionar las letras alfabeticamente?", respuestaCorrecta6);
+
+        Ronda ronda6 = new Ronda();
+        ronda6.agregarPregunta(unaPregunta6);
+        ronda6.agregarOpciones(opciones6);
+
+        //-------------------------------------------//
+       // verdaderoFalso; Penalidad; kashoot was sent to nil?;/verdadero;falso
+
+        Opcion opcionCorrecta71 = new Opcion("Verdadero");
+        Opcion opcionIncorrecta71 = new Opcion("Falso");
+
+        ArrayList <Opcion> opciones7 = new ArrayList<Opcion>();
+        opciones7.add(opcionCorrecta71);
+        opciones7.add(opcionIncorrecta71);
+
+        Respuesta respuestaCorrecta7 = new Respuesta();
+        respuestaCorrecta7.agregarOpcion(opcionCorrecta31);
+
+        Pregunta unaPregunta7 = new Pregunta(new ModalidadSinOrden(new Penalidad()), "Kashoot was sent to nil?", respuestaCorrecta7);
+
+        Ronda ronda7 = new Ronda();
+        ronda7.agregarPregunta(unaPregunta7);
+        ronda7.agregarOpciones(opciones7);
+
+        //-------------------------------------------//
+       // verdaderoFalso; Penalidad; los michis son malos?; verdadero;/falso
+
+        Opcion opcionCorrecta81 = new Opcion("Verdadero");
+        Opcion opcionIncorrecta81 = new Opcion("Falso");
+
+        ArrayList <Opcion> opciones8 = new ArrayList<Opcion>();
+        opciones8.add(opcionCorrecta81);
+        opciones8.add(opcionIncorrecta81);
+
+        Respuesta respuestaCorrecta8 = new Respuesta();
+        respuestaCorrecta8.agregarOpcion(opcionCorrecta81);
+
+        Pregunta unaPregunta8 = new Pregunta(new ModalidadSinOrden(new Penalidad()), "Los michis son malos?", respuestaCorrecta8);
+
+        Ronda ronda8 = new Ronda();
+        ronda8.agregarPregunta(unaPregunta8);
+        ronda8.agregarOpciones(opciones8);
+
+        //-------------------------------------------//
 
         Kashoot kashoot = new Kashoot();
         kashoot.agregarRonda(ronda1);
         kashoot.agregarRonda(ronda2);
+        kashoot.agregarRonda(ronda3);
+        kashoot.agregarRonda(ronda4);
+        kashoot.agregarRonda(ronda5);
+        kashoot.agregarRonda(ronda6);
+        kashoot.agregarRonda(ronda7);
+        kashoot.agregarRonda(ronda8);
 
         return kashoot;
     }
