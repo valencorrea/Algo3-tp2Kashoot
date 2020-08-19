ee4370a6c4c84657c225365157aa8d3ed13ec18c
stephanieizquierdo <sizquierdo@fi.uba.ar>
2020-08-18 22:00:27 -0300

    Agregamos label con puntajes y nombres

--- Kashoot/src/main/java/edu/fiuba/algo3/modelo/Kashoot.java
+++ Kashoot/src/main/java/edu/fiuba/algo3/modelo/Kashoot.java
@@ -33,8 +33,8 @@ public class Kashoot  {
 
         Jugador jugadorActual = jugadores.poll();
 
-        jugadores.add(jugadorActual);
-
+        jugadores.add(jugadorActual); //jugadores es una cola, lo pone al final
+        System.out.println("Actualice Jugador");
     }
 
     public Jugador obtenerJugadorActual(){
