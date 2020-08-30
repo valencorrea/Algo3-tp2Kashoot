f959f5280d0f7253fb582cac771e411f9f5043c4
valen <vcorrea@fi.uba.ar>
2020-08-29 23:46:57 -0300

    ya funciona bajar volumen

--- Kashoot/src/main/java/edu/fiuba/algo3/vista/Juego.java
+++ Kashoot/src/main/java/edu/fiuba/algo3/vista/Juego.java
@@ -29,7 +29,7 @@ import java.util.ArrayList;
     @Override
     public void start(Stage stage) throws Exception {
 
-        double volumen = 0.1;
+        double volumen = 0.4;
         MediaPlayer musica = agregarMusica(volumen);
 
         stage.setTitle("Kashoot");
